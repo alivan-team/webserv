265df974d1c860c8555cb391acecefd092d01aea
12Ivan03 <144702214+12Ivan03@users.noreply.github.com>
2026-06-21 18:05:06 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -17,7 +17,26 @@ public:
     std::vector<LocationConfig> locations;
 };
 
-int main() {
+int main(int argc, char **argv)
+{
+    if (argc != 2)
+    {
+        std::cerr << "Usage: ./webserv <config_file>\n";
+        return 1;
+    }
 
-    return 0;
+    Config config;
+
+    try
+    {
+        config = ConfigParser::parse(argv[1]);
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << "Config error: " << e.what() << "\n";
+        return 1;
+    }
+
+    // use config.servers[0].port for socket/bind/listen
+    // then use config later when handling requests
 }
