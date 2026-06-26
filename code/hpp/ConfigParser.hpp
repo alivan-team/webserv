f0fc887479aef16a130114832918b0f6290c09f8
Ivan <ivan.pavlov1203@gmail.com>
2026-06-26 11:30:26 +0200

    parser map

--- code/hpp/ConfigParser.hpp
+++ code/hpp/ConfigParser.hpp
@@ -14,7 +14,7 @@ class ConfigParser {
 		typedef void (ServerConfig::*Setter)(const std::string&);
 
     	std::map<std::string, Setter> setters;
-		
+
 	    std::vector<ServerConfig> servers;
 	
 	public:
@@ -25,3 +25,70 @@ class ConfigParser {
 };
 
 #endif
+
+
+// ok and now here: 
+
+
+
+// void ConfigParser::parse(const std::string& filename)
+
+// {
+
+//     std::ifstream file(filename.c_str());
+
+
+
+//     if (!file) {
+
+//         throw std::runtime_error("Cannot open config file");
+
+// 	}
+
+// 	std::vector<std::string> configTokens = tokenize(file);
+
+//     // MAP -> asdad array [1,1] =  ([]"listen", *setPort], ["server_name", *setServerName])
+
+//     for (int i = 0; i < configTokens.size(); i++) {
+
+
+
+//         if (configTokens[i] == "server" && i + 1 < configTokens.size() && configTokens[i + 1] == "{") {
+
+            
+
+//             ServerConfig server;
+
+//             i += 2;
+
+           
+
+//             while (i  < configTokens.size() && configTokens[i] != "}" ) {
+
+                
+
+//                 try {
+
+
+
+//                 } catch(std::string exeption ) {
+
+//                     throw ;
+
+//                 }
+
+//             }
+
+
+
+//             servers.push_back(server);
+
+//         }
+
+    
+
+//     }
+
+
+
+// in try I have to put call the setters
